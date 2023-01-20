66b529b55e694cfb6dbbd281465270e7ca157bd3
natalie-maks <natasha.maxymenko@gmail.com>
2023-01-20 22:51:48 +0000

    Deleted footer

--- src/App.js
+++ src/App.js
@@ -5,13 +5,6 @@ function App() {
   return (
     <div className="App">
       <Dictionary />
-
-      <footer>
-        <a href="https://github.com/natalie-maks/dictionary-app-react">
-          Open-source code
-        </a>
-        , by Natalia Maksymenko
-      </footer>
     </div>
   );
 }
