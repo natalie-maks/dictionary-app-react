cd89a3002fa685f55aa6cc8b41e5fa9ce11b077e
natalie-maks <natasha.maxymenko@gmail.com>
2023-01-29 22:44:22 +0000

    Added check in Pictures

--- src/Pictures.js
+++ src/Pictures.js
@@ -2,24 +2,28 @@ import React from "react";
 import { v1 as uuidv1 } from "uuid";
 
 export default function Pictures(props) {
-  if (props.pictures.length === 6) {
-    return (
-      <section className="Pictures">
-        <div className="row">
-          {props.pictures.map(function (picture) {
-            return (
-              <div key={uuidv1()} className="col-md-4 p-2">
-                <img
-                  className="img-fluid"
-                  src={picture.src.landscape}
-                  alt={picture.alt}
-                />
-              </div>
-            );
-          })}
-        </div>
-      </section>
-    );
+  if (props.pictures) {
+    if (props.pictures.length === 6) {
+      return (
+        <section className="Pictures">
+          <div className="row">
+            {props.pictures.map(function (picture) {
+              return (
+                <div key={uuidv1()} className="col-md-4 p-2">
+                  <img
+                    className="img-fluid"
+                    src={picture.src.landscape}
+                    alt={picture.alt}
+                  />
+                </div>
+              );
+            })}
+          </div>
+        </section>
+      );
+    } else {
+      return null;
+    }
   } else {
     return null;
   }
